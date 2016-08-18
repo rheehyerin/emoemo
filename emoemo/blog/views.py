dcf4146191a12301d292fedb67933558f6ffea6d
Lee Sangheun <heun0108@gmail.com>
2016-08-19 00:33:42 +0900

    blog 앱 views.post_detail 생성 & url 경로 설정

--- emoemo/blog/views.py
+++ emoemo/blog/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
     post_list = Post.objects.all()
     context = {"post_list":post_list}
     return render(request, 'blog/index.html', context)
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {
+        "post":post,
+    }
+    return render(request, 'blog/post_detail.html', context)
 
