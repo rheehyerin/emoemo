93907097f6d8124d913165c1fe43972a1fcf2f96
Lee Sangheun <heun0108@gmail.com>
2016-08-19 03:47:27 +0900

    blog 앱 comment 추가 & 수정 & 삭제 기능 추가

--- emoemo/blog/urls.py
+++ emoemo/blog/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^(?P<post_id>\d+)/$', views.post_detail, name='post_detail'),
-
+    url(r'^create/$', views.post_create, name='post_create'),
+    url(r'^(?P<post_id>\d+)/comment/new/$', views.comment_create, name='comment_create'),
+    url(r'^(?P<post_id>\d+)/comment/new/(?P<comment_id>\d+)/edit/$', views.comment_update, name='comment_update'),
+    url(r'^(?P<post_id>\d+)/comment/new/(?P<comment_id>\d+)/delete/$', views.comment_delete, name='comment_delete'),
 ]
--- emoemo/blog/views.py
+++ emoemo/blog/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
 from django.contrib import messages
 from django.http import HttpResponseRedirect
+from .forms import PostForm, CommentForm
 
 def post_list(request):
     post_list = Post.objects.all()
-    context = {"post_list":post_list}
+    form = PostForm(request.POST or None, request.FILES or None)
+    context = {
+            "post_list":post_list,
+            "form":form
+            }
     return render(request, 'blog/index.html', context)
 
 def post_detail(request, post_id):
@@ -17,10 +22,64 @@ def post_detail(request, post_id):
 
 def post_create(request):
     if request.method == 'POST':
-        instance = form.save(commit=False)
-        instance.author = request.user
-        instance.save()
-        messages.success(request, "Successfully Created")
-        pass
+        form = PostForm(request.POST or None, request.FILES or None)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
+            messages.success(request, "Successfully Created")
+            return redirect("blog:post_list")
+    else:
+        form = PostForm()
+
+    context = {
+        "form":form,
+    }
+    return render(request, 'blog/post_form.html', context)
 
 
+def comment_create(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST or None, request.FILES or None)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.post = post
+            instance.save()
+            return redirect('blog:post_detail', post_id)
+    else:
+        form = CommentForm()
+
+    context = {
+        "form":form,
+    }
+
+    return render(request, 'blog/comment_form.html', context)
+
+def comment_update(request, post_id, comment_id):
+    post = get_object_or_404(Post, pk=post_id)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST or None, request.FILES or None, instance=comment)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('blog:post_detail', post_id)
+    else:
+        form = CommentForm(instance=comment)
+    context = {
+        "form":form,
+    }
+
+    return render(request, 'blog/comment_form.html', context)
+
+def comment_delete(request, comment_id):
+    instance = get_object_or_404(Comment, comment_id)
+    instance.delete()
+    messages.success(request, "삭제 완료")
+    return redirect("blog:post_detail")
