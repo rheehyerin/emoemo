de915d6dd0b5f74250d4bfc141b5bfe12fd0c642
김병재 <kbjay21@gmail.com>
2016-08-20 00:50:00 +0900

    font change over and over and over and over and over

--- emoemo/emoemo/context_processors.py
+++ emoemo/emoemo/context_processors.py
@@ -3,5 +3,6 @@ def themes(request):
     return {
         'themes': {
             'background_colors': ['#B30039', '#FFA2C0', '#A2FFC0', '#F6FFA2'],
+            'fonts': ['font1', 'font2', 'font3'],
         },
     }
